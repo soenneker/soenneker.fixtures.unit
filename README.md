46da26d69289fa4459956702d95bb64756eb4696
Jake Soenneker <jake@soenneker.com>
2023-03-23 19:48:27 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Fixtures.Unit.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Fixtures.Unit/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.fixtures.unit/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.fixtures.unit/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Fixtures.Unit.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Fixtures.Unit/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Fixtures.Unit
+### A base xUnit fixture providing injectable log output and DI mechanisms like IServiceCollection and ServiceProvider
+
+## Installation
+
+```
+Install-Package Soenneker.Fixtures.Unit
+```
---
+++ src/UnitFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.XUnit.Injectable;
+using Serilog.Sinks.XUnit.Injectable.Abstract;
+using Serilog.Sinks.XUnit.Injectable.Extensions;
+using Xunit;
+
+namespace Soenneker.Fixtures.Unit;
+
+/// <summary>
+/// A base xUnit fixture providing injectable log output and DI mechanisms like IServiceCollection and ServiceProvider
+/// </summary>
+public abstract class UnitFixture : IAsyncLifetime
+{
+    public ServiceProvider? ServiceProvider { get; set; }
+
+    protected IServiceCollection Services { get; set; }
+
+    public UnitFixture()
+    {
+        var injectableTestOutputSink = new InjectableTestOutputSink();
+
+        // this needs to remain in constructor because of derivations
+        Services = new ServiceCollection();
+
+        Services.AddSingleton<IInjectableTestOutputSink>(injectableTestOutputSink);
+
+        ILogger serilogLogger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Async(a => a.InjectableTestOutput(injectableTestOutputSink))
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        Log.Logger = serilogLogger;
+    }
+
+    public virtual Task InitializeAsync()
+    {
+        ServiceProvider = Services.BuildServiceProvider();
+
+        return Task.CompletedTask;
+    }
+    
+    public virtual async Task DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (ServiceProvider != null)
+            await ServiceProvider.DisposeAsync();
+    }
+}
