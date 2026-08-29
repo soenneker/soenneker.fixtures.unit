f6eaef6893e7b87b1cb6ecf8c546b91082522ff7
soenneker <jake@soenneker.com>
2026-08-29 14:50:40 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Fixtures.Unit/UnitFixture.cs
+++ src/Soenneker.Fixtures.Unit/UnitFixture.cs
@@ -13,7 +13,7 @@ using Soenneker.Utils.AutoBogus.Config;
 
 namespace Soenneker.Fixtures.Unit;
 
-///<inheritdoc cref="IUnitFixture"/>
+/// <inheritdoc cref="IUnitFixture"/>
 public abstract class UnitFixture : IUnitFixture
 {
     private readonly InjectableTestOutputSink? _sink;
